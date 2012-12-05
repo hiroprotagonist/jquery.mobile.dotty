2b1a68344e24b5d47e8867b6c7e9d1970eac12ad
Stefan Gebhardt <stefan.gebhardt@finux.net>
2012-12-05 12:41:26 +0100

    More OOP, less crap

--- jquery.mobile.dotty.js
+++ jquery.mobile.dotty.js
@@ -2,10 +2,18 @@
  * requires underscore
  */
 (function($, window, undefined) {
+
 	$.widget("mobile.dotty", $.mobile.widget, {
+		resize_event: ('onorientationchange' in window) ?'orientationchange' :'resize',
 		total: 0, // Number of Dots
 		highlight_index: 0,
+		fullsize_painter: null,
+		smallsize_painter: null,
 		_init: function () {
+			// Caching painter instances
+			this.fullsize_painter = FullsizePainter( this );
+			this.smallsize_painter = SmallsizePainter( this );
+			
 			// There can only be one of my kind
 			if ( this.element.data('initialized') ) {
 				return;
@@ -13,9 +21,12 @@
 			this.element
 				.addClass( 'dotty' )
 				.data( 'initialized', true );
-			$(window).resize($.proxy(function() {
+			$(window).bind( this.resize_event, $.proxy(function() {
 				this.draw( this.highindex, this.total, true );
 			}, this));
+		},
+		hide: function() {
+			this._clearui();
 		},
 		draw: function( nof, total, forceredraw ) {
 			this.highindex = nof;
@@ -23,53 +34,60 @@
 			// Optimization
 			if ( this.total !== total || forceredraw) {
 				this.total = total;
-				painter.drawdots( nof, total );
+				painter.drawdots( total );
 				painter.highlight( nof );
 			} else {
 				painter.highlight( nof );
 			}
 		},
 		_getPainter: function( totalDots ) {
-			var margin = 150;
-			if ( window.innerWidth > ( totalDots * 24 + margin ) ) {
-				return {
-					drawdots: $.proxy(this._drawdots_pretty, this),
-					highlight: $.proxy(this._highlight_pretty, this)
-				};
-			} else {
-				return {
-					drawdots: $.proxy(this._drawdots_spartan, this),
-					highlight: $.proxy(this._highlight_spartan, this)
-				};
-			}
-		},
-		_drawdots_pretty: function ( nof, total ) {
-			this._clearui();
-			var el = this.element;
-			_(total).times( function( idx ) {
-				el.append( $('<div class="dot">') );
-			});
-		},
-		_highlight_pretty: function ( n ) {
-			$( '.pointer', this.element ).removeClass( 'pointer' );
-			$( '.dot:nth-child(' + n + ')', this.element).addClass( 'pointer' );						
-		},
-		_drawdots_spartan: function( nof, total ) {
-			this._clearui();
-			var el = this.element
-			  , n = $('<span class="n">').html( nof )
-			  , seperator = $('<span class="seperator">').html( '/' )
-			  , total = $('<span class="total">').html( total );
-			el.append([n,seperator,total]);
-		},
-		_highlight_spartan: function ( n ) {
-			$( '.n', this.element ).html( n );
+			return ( window.innerWidth > ( totalDots * 24 + 150 ) )
+				? this.fullsize_painter
+				: this.smallsize_painter;
 		},
 		_clearui: function ( ) {
 			this.element.empty();
 		}
 	});
 
+	function FullsizePainter( widget ) {
+		function drawdots ( total ) {
+			widget._clearui();
+			var el = widget.element;
+			_(total).times( function( idx ) {
+				el.append( $('<div class="dot">') );
+			});
+		}
+		function highlight( n ) {
+			$( '.pointer', widget.element ).removeClass( 'pointer' );
+			if ( _.isNumber(n) ) {
+				$( '.dot:nth-child(' + n + ')', widget.element).addClass( 'pointer' );
+			}
+		}
+		return {
+			drawdots: drawdots,
+			highlight: highlight
+		};
+	}
+
+	function SmallsizePainter( widget ) {
+		function drawNumbers( total ) {
+			widget._clearui();
+			var el = widget.element
+			  , nDIV = $('<span class="n">')
+			  , seperatorDIV = $('<span class="seperator">').html( '/' )
+			  , totalDIV = $('<span class="total">').html( total );
+			el.append([nDIV,seperatorDIV,totalDIV]);
+		}
+		function drawCurrentPageNumber( n ) {
+			$( '.n', this.element ).html( n );
+		}
+		return {
+			drawdots: drawNumbers,
+			highlight: drawCurrentPageNumber
+		};
+	}
+	
 	$(document).ready( function() {
 		var pager = $( '<div data-role="dotty">' );
 		$( 'body' ).append(pager);
@@ -80,5 +98,3 @@
 	});
 	
 })(jQuery, this);
-
-
